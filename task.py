29e724f485753fc8d9a39df4ee9b737df6e7265c
darvenommm <darvenommm@gmail.com>
2023-09-13 11:51:27 +0300

    feat: add a function that can calculate a char from a binary number

---
+++ task.py
@@ -0,0 +1,23 @@
+from functools import reduce
+
+
+def binary_to_decimal(binary_number: str) -> int:
+    return int(binary_number, 2)
+
+
+def get_char_by_binary_number(binary_number: str) -> str:
+    return chr(binary_to_decimal(binary_number))
+
+
+chars = [
+    '1001000', '1100001', '1110000', '1110000', '1111001',
+    '1000000', '1110000', '1110010', '1101111', '1100111',
+    '1110010', '1100001', '1101101', '1101101', '1110010',
+    '1110011', '1000000', '1100001', '1111001', '1000001',
+]
+
+
+def reduce_chars(result, char): return result + get_char_by_binary_number(char)
+
+
+print(reduce(reduce_chars, chars, ''))
